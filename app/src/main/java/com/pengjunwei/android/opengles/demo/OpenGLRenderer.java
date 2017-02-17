68f42177b2ddfe4f95c3d929ad84996bbe84ec29
wiki <wiki.peng@qq.com>
2017-02-17 17:43:40 +0800

    绘制3个正方形A,B,C。进行缩放变换，使的B比A小50%，C比B小50%。 然后以屏幕中心逆时针旋转A，B以A为中心顺时针旋转，C以B为中心顺时针旋转同时以自己中心高速逆时针旋转

--- app/src/main/java/com/pengjunwei/android/opengles/demo/OpenGLRenderer.java
+++ app/src/main/java/com/pengjunwei/android/opengles/demo/OpenGLRenderer.java
@@ -13,7 +13,7 @@ import javax.microedition.khronos.opengles.GL10;
  */
 public class OpenGLRenderer implements GLSurfaceView.Renderer {
     protected Square square;
-
+    protected int    angle = 90;
 
     public OpenGLRenderer() {
         square = new Square();
@@ -37,14 +37,66 @@ public class OpenGLRenderer implements GLSurfaceView.Renderer {
 
 
     public void onDrawFrame(GL10 gl) {
-        // Clears the screen and depth buffer.
-        gl.glClear(GL10.GL_COLOR_BUFFER_BIT | // OpenGL docs.
-                GL10.GL_DEPTH_BUFFER_BIT);
+//        // Clears the screen and depth buffer.
+//        gl.glClear(GL10.GL_COLOR_BUFFER_BIT | // OpenGL docs.
+//                GL10.GL_DEPTH_BUFFER_BIT);
+//
+//        gl.glLoadIdentity();
+//        gl.glTranslatef(0, 0, -4);
+//        // Draw our square.
+//        square.draw(gl); // ( NEW )
 
+        // Clears the screen and depth buffer.
+        gl.glClear(GL10.GL_COLOR_BUFFER_BIT
+                | GL10.GL_DEPTH_BUFFER_BIT);
+        // Replace the current matrix with the identity matrix
         gl.glLoadIdentity();
-        gl.glTranslatef(0, 0, -4);
-        // Draw our square.
-        square.draw(gl); // ( NEW )
+        // Translates 10 units into the screen.
+        gl.glTranslatef(0, 0, -10);
+
+        // SQUARE A
+        // Save the current matrix.
+        gl.glPushMatrix();
+        // Rotate square A counter-clockwise.
+        gl.glRotatef(angle, 0, 0, 1);
+        // Draw square A.
+        square.draw(gl);
+        // Restore the last matrix.
+        gl.glPopMatrix();
+
+        // SQUARE B
+        // Save the current matrix
+        gl.glPushMatrix();
+        // Rotate square B before moving it,
+        //making it rotate around A.
+        gl.glRotatef(-angle, 0, 0, 1);
+        // Move square B.
+        gl.glTranslatef(2, 0, 0);
+        // Scale it to 50% of square A
+        gl.glScalef(.5f, .5f, .5f);
+        // Draw square B.
+        square.draw(gl);
+
+        // SQUARE C
+        // Save the current matrix
+        gl.glPushMatrix();
+        // Make the rotation around B
+        gl.glRotatef(-angle, 0, 0, 1);
+        gl.glTranslatef(2, 0, 0);
+        // Scale it to 50% of square B
+        gl.glScalef(.5f, .5f, .5f);
+        // Rotate around it's own center.
+        gl.glRotatef(angle * 10, 0, 0, 1);
+        // Draw square C.
+        square.draw(gl);
+
+        // Restore to the matrix as it was before C.
+        gl.glPopMatrix();
+        // Restore to the matrix as it was before B.
+        gl.glPopMatrix();
+
+        // Increse the angle.
+        angle++;
     }
 
 
